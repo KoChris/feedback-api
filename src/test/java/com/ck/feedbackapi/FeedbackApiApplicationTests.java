ca3a0b37b5ca4c898707fff2163725e20329b6d2
kochris <chris.ko791@gmail.com>
2020-07-27 23:20:26 -0400

    Spring starter code

---
+++ src/test/java/com/ck/feedbackapi/FeedbackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ck.feedbackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
